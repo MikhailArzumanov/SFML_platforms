a0fb2dc53781a8b966c597c79d0ed3dcefb89499
Арзуманов Михаил <marzumanov@softrust.ru>
2022-05-19 17:53:54 +0300

    header of ExamplePlatform class was moved to separate file

---
+++ ExamplePlatform.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "Platform.hpp"
+
+extern const point examplePlatformDims;
+
+class ExamplePlatform : public Platform {
+private:
+	sf::RectangleShape shape;
+public:
+	ExamplePlatform(sf::RectangleShape shape_, point position);
+	void draw();
+	void tick();
+	bool is_living();
+};
--- SFML_platforms.cpp
+++ SFML_platforms.cpp
@@ -3,31 +3,16 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 #include "window.hpp"
-#include "Platform.hpp"
-
+#include "ExamplePlatform.hpp"
 
 sf::RenderWindow* window;
 
 
-const point examplePlatformDims = { 120, 12 };
 const sf::Color examplePlatformColor = sf::Color(0x70, 0x70, 0x70);
 sf::RectangleShape exampleRectangle = sf::RectangleShape(examplePlatformDims);
 
-class ExamplePlatform : public Platform {
-public:
-	ExamplePlatform(point position) : Platform(position, examplePlatformDims) {}
-	void draw() {
-		point lu_corner = p - dims / 2;
-		lu_corner >> exampleRectangle;
-	}
-	void tick() {
-		p.x++;
-		p.y++;
-	}
-	bool is_living() { return true; }
-};
 
-ExamplePlatform platform = ExamplePlatform({ 0, 0 });
+ExamplePlatform platform = ExamplePlatform(exampleRectangle, { 0, 0 });
 
 
 int tickCounter = 0;
