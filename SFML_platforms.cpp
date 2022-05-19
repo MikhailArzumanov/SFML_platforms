b952be4510fd55d9482894ab7a75929d51b4e552
Арзуманов Михаил <marzumanov@softrust.ru>
2022-05-19 17:10:45 +0300

    point struct was added

--- SFML_platforms.cpp
+++ SFML_platforms.cpp
@@ -4,7 +4,19 @@
 
 sf::RenderWindow* window;
 
+struct point {
+	float x, y;
+	operator sf::Vector2f() const {
+		return sf::Vector2f({ x, y });
+	}
 
+	point operator-(point another) {
+		return { x - another.x, y - another.y };
+	}
+	point operator/(float scalar) {
+		return { x / scalar, y / scalar };
+	}
+};
 
 int main(){
 	window = new sf::RenderWindow(sf::VideoMode(700, 313), "Debug window");
