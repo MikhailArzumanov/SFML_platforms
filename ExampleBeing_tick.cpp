dea06c65d6a69ca4cd75be121f50a12d2b158bff
Арзуманов Михаил <marzumanov@softrust.ru>
2022-05-23 17:31:32 +0300

    windage was added for ExampleBeing

--- ExampleBeing_tick.cpp
+++ ExampleBeing_tick.cpp
@@ -1,11 +1,16 @@
 #include "ExampleBeing.hpp"
+#include <iostream>
 
-const float gravityCoefficient = 1.f;
+const float windage = 12.f;
+const float gravityCoefficient = 4.f;
+
 
 void ExampleBeing::tick() {
 	a.y += gravityCoefficient;
 	v += a;
 	a = {0,0};
 	p += v;
+	std::cout <<"dy = " <<v.y <<"\n";
+	v.y /= windage;
 	//...
 }
