37bfc138025caaa50eb91b0396ec2d9b4b3576bd
Арзуманов Михаил <marzumanov@softrust.ru>
2022-05-23 17:39:08 +0300

    tick system was reorganised

--- tick.cpp
+++ tick.cpp
@@ -1,11 +1,12 @@
 #include "main_functions.hpp"
 #include "platforms.hpp"
 #include "beings.hpp"
+#include "theClock.hpp"
 
 #define in :
-int tickCounter = 0;
 
 void tick() {
+	theClock.restart();
 	for(auto platform in platforms)
 		platform->tick();
 	for (auto being in beings) {
@@ -13,15 +14,14 @@ void tick() {
 		for (auto platform in platforms)
 			if (being->isCollindingWith(platform))
 				being->collide(platform);
-
 		for (auto another in beings)
 			if (being != another && being->isCollindingWith(another))
 				being->collide(another);
 	}
-	tickCounter = 0;
 }
 
+
 void checkTick() {
-	if (tickCounter++ > 1000)
+	if (theClock.getElapsedTime().asSeconds() >= 1/60.f)
 		tick();
 }
