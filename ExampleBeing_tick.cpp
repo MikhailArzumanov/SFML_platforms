80616efea1f162c52a1fc7e59c50c984d676f387
Арзуманов Михаил <marzumanov@softrust.ru>
2022-05-24 18:12:21 +0300

    v.y -- nulling was added at collision, gravitation and jump coefficients were repaired

--- ExampleBeing_tick.cpp
+++ ExampleBeing_tick.cpp
@@ -1,10 +1,10 @@
 #include "ExampleBeing.hpp"
 #include <iostream>
 
-const float gravityCoefficient = 0.12f;
+const float gravityCoefficient = 0.23f;
 const float frictionCoefficient = 12.f;
 const float speed = 3.f;
-const float yforce = 0.2856f; 
+const float yforce = 8.712f; 
 int cooldown = 0;
 
 
@@ -19,13 +19,13 @@ void ExampleBeing::tick() {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && cooldown == 0) {
 		v.y = -yforce; cooldown = 120;
 	}
-	if(cooldown > 0) cooldown--;
+	if(cooldown > 0) std::cout <<"cooldown: " <<cooldown-- <<"\n";
 
 	a.y += gravityCoefficient;
 	v += a;
 	a = { 0.f,0.f};
 	p += v;
-	std::cout << "dy = " << v.y << "\n";
+	//std::cout << "dy = " << v.y << "\n";
 	//...
 	//...
 	if (lastCollidable != nullptr) {
@@ -33,6 +33,7 @@ void ExampleBeing::tick() {
 		float beingLowerY = p.y + dims.y / 2;
 		if (beingLowerY > platformUpperY) {
 			p.y = platformUpperY - dims.y / 2 + 0.12f;
+			v.y = v.y < 0 ? v.y : 0;
 			v.x /= frictionCoefficient;
 		}
 		lastCollidable = nullptr;
