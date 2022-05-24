d32e58afebab8e33854a4129ea8e5212b485fdd2
Арзуманов Михаил <marzumanov@softrust.ru>
2022-05-24 17:29:12 +0300

    onKeyPressed event was implemented for ExampleBeing

--- ExampleBeing_onKeyPressed.cpp
+++ ExampleBeing_onKeyPressed.cpp
@@ -1,12 +1,19 @@
 #include "ExampleBeing.hpp"
 
+const float speed = 12.f;
+
 void ExampleBeing::onKeyPressed(sf::Keyboard::Key key) {
 	switch (key) {
 	case sf::Keyboard::W:
+		//v.y = -speed;
+		break;
 	case sf::Keyboard::A:
+		v.x = -speed;
+		break;
 	case sf::Keyboard::S:
+		break;
 	case sf::Keyboard::D:
+		v.x = speed;
 		break;
 	}
-
 }
