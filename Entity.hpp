2943bf891344e9b02b9209b93b50b6d210cab19d
Арзуманов Михаил <marzumanov@softrust.ru>
2022-06-03 17:23:36 +0300

    Events were separated to an interface

--- Entity.hpp
+++ Entity.hpp
@@ -6,25 +6,18 @@
 #include "EntityType.hpp"
 #include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/Event.hpp>
+#include "Eventable.hpp"
 
-class Entity : public Drawable, public Living, public Ticking {
+class Entity : public Drawable, public Living, public Ticking, public Eventable {
 protected:
 	EntityType type;
 	point p, v, a, dims;
 	Entity(point position, point dims_) {
 		p = position; dims = dims_;
 	}
+
 public:
 	point getP(){return p;}
 	point getDims(){return dims;}
 	EntityType getType(){return type;}
-
-	virtual void onKeyPressed(sf::Keyboard::Key key) {}
-	virtual void onKeyReleased(sf::Keyboard::Key key) {}
-	virtual void onMouseButtonPressed(sf::Event::MouseButtonEvent theEvent) {}
-	virtual void onMouseButtonReleased(sf::Event::MouseButtonEvent theEvent) {}
-	virtual void onMouseMoved(sf::Event::MouseMoveEvent theEvent) {}
-	virtual void onJoystickButtonPressed(sf::Event::JoystickButtonEvent theEvent) {}
-	virtual void onJoystickButtonReleased(sf::Event::JoystickButtonEvent theEvent) {}
-
 };
