8d3ad315375fb95eda61bc0157407c78884510f0
Арзуманов Михаил <marzumanov@softrust.ru>
2022-06-06 14:15:06 +0300

    handleEvents function was refactored

--- handleEvents.cpp
+++ handleEvents.cpp
@@ -10,25 +10,25 @@ void handleEvents() {
 			window->close();
 			break;
 		case sf::Event::KeyPressed:
-			for(auto being : eventables) being->onKeyPressed(theEvent.key.code);
+			for(auto eventable : eventables) eventable->onKeyPressed(theEvent.key.code);
 			break;
 		case sf::Event::KeyReleased:
-			for (auto being : eventables) being->onKeyReleased(theEvent.key.code);
+			for (auto eventable : eventables) eventable->onKeyReleased(theEvent.key.code);
 			break;
 		case sf::Event::MouseButtonPressed:
-			for (auto being : eventables) being->onMouseButtonPressed(theEvent.mouseButton);
+			for (auto eventable : eventables) eventable->onMouseButtonPressed(theEvent.mouseButton);
 			break;
 		case sf::Event::MouseButtonReleased:
-			for (auto being : eventables) being->onMouseButtonReleased(theEvent.mouseButton);
+			for (auto eventable : eventables) eventable->onMouseButtonReleased(theEvent.mouseButton);
 			break;
 		case sf::Event::MouseMoved:
-			for (auto being : eventables) being->onMouseMoved(theEvent.mouseMove);
+			for (auto eventable : eventables) eventable->onMouseMoved(theEvent.mouseMove);
 			break;
 		case sf::Event::JoystickButtonPressed:
-			for (auto being : eventables) being->onJoystickButtonPressed(theEvent.joystickButton);
+			for (auto eventable : eventables) eventable->onJoystickButtonPressed(theEvent.joystickButton);
 			break;
 		case sf::Event::JoystickButtonReleased:
-			for (auto being : eventables) being->onJoystickButtonReleased(theEvent.joystickButton);
+			for (auto eventable : eventables) eventable->onJoystickButtonReleased(theEvent.joystickButton);
 			break;
 		}
 	}
